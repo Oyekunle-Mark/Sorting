3ac63fdbc296a92e669a08c917cac3cacd226149
Oyekunle Oloyede <oyekunlemac@gmail.com>
2019-10-07 19:34:30 +0100

    Added the count sort algorithm

--- src/iterative_sorting/iterative_sorting.py
+++ src/iterative_sorting/iterative_sorting.py
@@ -1,14 +1,10 @@
 # TO-DO: Complete the selection_sort() function below
 def selection_sort(arr):
-    # loop through n-1 elements
     for i in range(0, len(arr) - 1):
         cur_index = i
         smallest_index = cur_index
-        # TO-DO: find next smallest element
-        # (hint, can do in 3 loc)
 
         for index in range(cur_index, len(arr)):
-            # TO-DO: swap
             if arr[index] < arr[smallest_index]:
                 arr[smallest_index], arr[index] = arr[index], arr[smallest_index]
 
@@ -36,7 +32,39 @@ def bubble_sort(arr):
 
 # STRETCH: implement the Count Sort function below
 
-
 def count_sort(arr, maximum=-1):
+    # return back an empty array
+    if len(arr) == 0:
+        return []
 
-    return arr
+    # will hold the sorted arr
+    result = [0] * len(arr)
+
+    # initialize a count array of greater in length than the maximum element in array by 1
+    high = max(arr)
+    count_array = [0 for i in range(high+1)]
+
+    # count the occurrence of each number in the array
+    for i in arr:
+        # this sort does not work with negative numbers
+        if i < 0:
+            return "Error, negative numbers not allowed in Count Sort"
+
+        count_array[i] += 1
+
+    # Modify the count array such that each element at each index
+    # stores the sum of previous counts
+    for j in range(1, len(count_array)):
+        count_array[j] += count_array[j-1]
+
+    # Find the index of each element of the original array in count array.
+    # This gives the cumulative count.
+    # Place the element at the index calculated.
+    for k in arr:
+        result[count_array[k] - 1] = k
+
+        # in case of multiple occurrence
+        # insert the number in the index to the left
+        count_array[k] -= 1
+
+    return result
