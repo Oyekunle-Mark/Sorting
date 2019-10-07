8b46f75e567c80de505cca6205f3717825156f76
Oyekunle Oloyede <oyekunlemac@gmail.com>
2019-10-07 20:43:40 +0100

    Completed the stretch goals in searching.py file

--- src/searching/searching.py
+++ src/searching/searching.py
@@ -1,30 +1,45 @@
-# STRETCH: implement Linear Search				
 def linear_search(arr, target):
-  
-  # TO-DO: add missing code
+    for i, item in enumerate(arr):
+        if item == target:
+            return i
 
-   return -1   # not found
+    return -1
 
 
-# STRETCH: write an iterative implementation of Binary Search 
 def binary_search(arr, target):
+    if len(arr) == 0:
+        return -1
 
-  if len(arr) == 0:
-    return -1 # array empty
-    
-  low = 0
-  high = len(arr)-1
+    low = 0
+    high = len(arr)-1
 
-  # TO-DO: add missing code
+    while low <= high:
+        midpoint = (low + high) // 2
 
-  return -1 # not found
+        if arr[midpoint] == target:
+            return arr.index(target)
+
+        if target < arr[midpoint]:
+            high = midpoint - 1
+        else:
+            low = midpoint + 1
+
+    return -1  # not found
 
 
-# STRETCH: write a recursive implementation of Binary Search 
 def binary_search_recursive(arr, target, low, high):
-  
-  middle = (low+high)//2
+    if len(arr) == 0:
+        return -1
 
-  if len(arr) == 0:
-    return -1 # array empty
-  # TO-DO: add missing if/else statements, recursive calls
+    if low > high:
+        return -1
+
+    middle = (low + high) // 2
+
+    if arr[middle] == target:
+        return arr.index(target)
+
+    if target < arr[middle]:
+        return binary_search_recursive(arr, target, low, middle - 1)
+    else:
+        return binary_search_recursive(arr, target, middle + 1, high)
